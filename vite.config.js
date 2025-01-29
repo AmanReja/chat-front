33b237f533a77b890cd822d3cdd116f4bbbf6cf3
Aman Reja <amnmolla742@gmail.com>
2025-01-29 07:23:33 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: `https://chat-backend-1-ukrx.onrender.com`,
+        target: `https://chat-backend-1-ukrx.onrender.com/`,
         changeOrigin: true
       }
     }
